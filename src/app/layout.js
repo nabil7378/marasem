3d16c75026cdb4775f7192c4d3b920f7a56ca6d3
nabil7378 <nabilmohamed966547@gmail.com>
2025-04-12 06:27:40 +0200

    meta

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,6 +10,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
       <body>
         {children}
         <BootstrapClient />
